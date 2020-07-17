3a7fe84db22f997beeb9188ac60ab223c361d846
RachelGao <gao_rq@outlook.com>
2020-07-17 14:19:07 +0800

    section 3.4 @Validated 对配置属性值校验

--- boot-launch/src/main/java/com/zimug/boot/launch/model/Family.java
+++ boot-launch/src/main/java/com/zimug/boot/launch/model/Family.java
@@ -1,15 +1,19 @@
 package com.zimug.boot.launch.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "family")
+@Validated
 public class Family {
 
+	@NotEmpty
 	private String familyName;   //成员变量名称要和yml配置项key一一对应
 	private Father father;
 	private Mother mother;
--- boot-launch/src/main/java/com/zimug/boot/launch/model/Father.java
+++ boot-launch/src/main/java/com/zimug/boot/launch/model/Father.java
@@ -2,9 +2,12 @@ package com.zimug.boot.launch.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 // 3. 爸爸有名字(name)和年龄（age）两个属性
 @Data
 public class Father {
     private String name;
+    @Min(21)
     private Integer age;
 }
