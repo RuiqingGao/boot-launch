c998d5abdfc8965969a591a83f113948f46a0965
RachelGao <gao_rq@outlook.com>
2020-07-24 00:04:24 +0800

    section 4.2 Spring JDBC多数据源的实现

--- boot-launch/src/main/java/com/zimug/boot/launch/dao/ArticleJDBCDAO.java
+++ boot-launch/src/main/java/com/zimug/boot/launch/dao/ArticleJDBCDAO.java
@@ -12,10 +12,14 @@ import java.util.List;
 public class ArticleJDBCDAO {
 
     @Resource
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate primaryJdbcTemplate;
 
     //保存文章
-    public void save(Article article) {
+    public void save(Article article,JdbcTemplate jdbcTemplate) {
+        if(jdbcTemplate == null){  //判断新增参数不能为空，如果为空使用primaryJdbcTemplate
+            jdbcTemplate= primaryJdbcTemplate;
+        }
+
         //jdbcTemplate.update适合于insert 、update和delete操作；
         jdbcTemplate.update("INSERT INTO article(author, title,content,create_time) values(?, ?, ?, ?)",
                 article.getAuthor(),
@@ -26,14 +30,20 @@ public class ArticleJDBCDAO {
     }
 
     //删除文章
-    public void deleteById(Long id) {
+    public void deleteById(Long id,JdbcTemplate jdbcTemplate) {
+        if(jdbcTemplate == null){  //判断新增参数不能为空，如果为空使用primaryJdbcTemplate
+            jdbcTemplate= primaryJdbcTemplate;
+        }
         //jdbcTemplate.update适合于insert 、update和delete操作；
         jdbcTemplate.update("DELETE FROM article WHERE id = ?", id);
 
     }
 
     //更新文章
-    public void updateById(Article article) {
+    public void updateById(Article article,JdbcTemplate jdbcTemplate) {
+        if(jdbcTemplate == null){  //判断新增参数不能为空，如果为空使用primaryJdbcTemplate
+            jdbcTemplate= primaryJdbcTemplate;
+        }
         //jdbcTemplate.update适合于insert 、update和delete操作；
         jdbcTemplate.update("UPDATE article SET author = ?, title = ? ,content = ?,create_time = ? WHERE id = ?",
                 article.getAuthor(),
@@ -45,14 +55,20 @@ public class ArticleJDBCDAO {
     }
 
     //根据id查找文章
-    public Article findById(Long id) {
+    public Article findById(Long id,JdbcTemplate jdbcTemplate) {
+        if(jdbcTemplate == null){  //判断新增参数不能为空，如果为空使用primaryJdbcTemplate
+            jdbcTemplate= primaryJdbcTemplate;
+        }
         //queryForObject用于查询单条记录返回结果
         return (Article) jdbcTemplate.queryForObject("SELECT * FROM article WHERE id=?",
                 new Object[]{id},new BeanPropertyRowMapper<>(Article.class));
     }
 
     //查询所有
-    public List<Article> findAll(){
+    public List<Article> findAll(JdbcTemplate jdbcTemplate){
+        if(jdbcTemplate == null){  //判断新增参数不能为空，如果为空使用primaryJdbcTemplate
+            jdbcTemplate= primaryJdbcTemplate;
+        }
         //query用于查询结果列表
         return (List<Article>) jdbcTemplate.query("SELECT * FROM article ",  new BeanPropertyRowMapper<>(Article.class));
     }
--- boot-launch/src/main/java/com/zimug/boot/launch/service/ArticleRestJDBCServiceImpl.java
+++ boot-launch/src/main/java/com/zimug/boot/launch/service/ArticleRestJDBCServiceImpl.java
@@ -4,6 +4,7 @@ import com.zimug.boot.launch.dao.ArticleJDBCDAO;
 import com.zimug.boot.launch.model.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,34 +18,40 @@ public class ArticleRestJDBCServiceImpl implements ArticleService {
 	@Autowired
 	ArticleJDBCDAO articleJDBCDAO;
 
+	@Resource
+	private JdbcTemplate primaryJdbcTemplate;
+	@Resource
+	private JdbcTemplate secondaryJdbcTemplate;
+
 	//服务层一般还要写一些业务逻辑
 	@Transactional
 	@Override
 	public Article saveArticle(Article article) {
-		articleJDBCDAO.save(article);
+		articleJDBCDAO.save(article,primaryJdbcTemplate);
+		articleJDBCDAO.save(article,secondaryJdbcTemplate);
 		return article;
 	}
 
 	@Override
 	public void deleteArticle(Long id) {
-		articleJDBCDAO.deleteById(id);
+		articleJDBCDAO.deleteById(id,null);
 
 	}
 
 	@Override
 	public void updateArticle(Article article) {
-		articleJDBCDAO.updateById(article);
+		articleJDBCDAO.updateById(article,null);
 
 	}
 
 	@Override
 	public Article getArticle(Long id) {
-		return articleJDBCDAO.findById(id);
+		return articleJDBCDAO.findById(id,null);
 
 	}
 
 	@Override
 	public List<Article> getAll() {
-		return articleJDBCDAO.findAll();
+		return articleJDBCDAO.findAll(null);
 	}
 }
